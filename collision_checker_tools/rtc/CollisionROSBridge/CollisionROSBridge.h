560a69c86b76cf3a8560246cf2095a98900ae00b
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2021-12-18 15:12:03 +0900

    fix

--- collision_checker_tools/rtc/CollisionROSBridge/CollisionROSBridge.h
+++ collision_checker_tools/rtc/CollisionROSBridge/CollisionROSBridge.h
@@ -19,6 +19,8 @@ protected:
   std::shared_ptr<urdf::Model> robot_urdf_;
   cnoid::BodyPtr robot_vrml_;
 
+  ros::NodeHandle nh; // これがないとうまく通信できなくなったり、CPU使用率100%になったりする
+
   std::string tf_prefix_;
 
   collision_checker_msgs::TimedCollisionSeq m_collisionRTM_;
